650a157a04b24fce7f87ac97650efa01667588cd
waramsey <46686986+waramsey@users.noreply.github.com>
2019-12-09 19:05:23 -0600

    Online, stomped a bug
    chat.js had a bug with the online, because the first line needed an update to which port it was connecting to.

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ var socket = require('socket.io');
 
 //App setup
 var app = express();
-var server = app.listen(80, function(){ //listening at port 4000
-    console.log('listening to requests on port 4000');
+var server = app.listen(8080, function(){ //listening at port 4000
+    console.log('listening to requests on port 8080');
 });
-
+const path = require("path");
 
 //Static files (what will be displayed on webpage)
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname,"public")));
 
 //Socket setup
 var io = socket(server);
@@ -19,73 +18,11 @@ var io = socket(server);
 io.on('connection', function(socket){
     console.log('made socket connection', socket.id);
 
-    socket.on('chat', function(data){ //Check in here if I have a bot command
-        //var output = document.getElementById('output');
+    socket.on('chat', function(data){
         io.sockets.emit('chat', data); //sockets refers to ALL connected sockets
-        if (data.message.charAt(0) == '/') {
-            simpleDice(data);
-        }
     });
 
     socket.on('typing', function(data){
        socket.broadcast.emit('typing', data);  //broadcasting sends the data to every OTHER client socket
     });
 });
-
-
-//Dice Bot Code
-function simpleDice(data) {
-    
-    var cmdLength = data.message.length; //length of the command
-    var i = 1; //position in the command
-
-    var numDice = "";
-    var typeDice = "";
-    var modifier = "";
-    var addSub; //true if addition, false if subtraction
-    
-
-    while (i < cmdLength && data.message.charAt(i) != 'd') {
-        numDice += data.message.charAt(i);
-        i++;
-    }
-
-    i++; //moves past 'd'
-
-    while (i < cmdLength && data.message.charAt(i) != '+' && data.message.charAt(i) != '-') {
-        typeDice += data.message.charAt(i);
-        i++;
-    }
-
-    if (data.message.charAt(i) == '+') {
-        addSub = true;
-    } else if (data.message.charAt(i) == '-') {
-        addSub = false;
-    }
-
-    while (i < cmdLength) {
-        modifier += data.message.charAt(i);
-        i++;
-    }
-
-    var numDice = parseInt(numDice);
-    var typeDice = parseInt(typeDice);
-    var modifier = parseInt(modifier);
-
-    var roll = 0;
-    for (var i = 0; i < numDice; i++) {
-        roll += Math.ceil(typeDice * Math.random());
-    }
-
-    if (typeof addSub === 'undefined') {
-        //do nothing :)
-    } else if (addSub === true) {
-        output.innerHTML += '<p><strong>DiceBot</strong>:  Rolled: ' + roll + '+' + modifier + '</p>';
-        roll += modifier;
-    } else if (addSub === false) {
-        output.innerHTML += '<p><strong>DiceBot</strong>:  Rolled: ' + roll + '' + modifier + '</p>';
-        roll += modifier;
-    }
-
-    output.innerHTML += '<p><strong>DiceBot</strong>:  Total: ' + roll + '</p>';
-}
--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 //Make Connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://localhost:8080');
 
 //Query DOM
 var message = document.getElementById('message');
@@ -45,7 +45,10 @@ socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.handle + '</strong>:  ' + data.message + '</p>';
     feedback.innerHTML = '';
 
-    //PREVIOUS LOCATION OF BOT CALL
+    if (data.message.charAt(0) == '/') {
+        //TODO: ASK SERVER FOR INFORMATION
+        simpleDice(data);
+    }
 
     output.scrollIntoView(false); //scrolls to bottom
     message.value = ''; //clears the message box
@@ -55,3 +58,60 @@ socket.on('typing', function(data){
     feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
     feedback.scrollIntoView(true); //scrolls to bottom
 });
+
+//Dice Bot Code
+function simpleDice(data) {
+    
+    var cmdLength = data.message.length; //length of the command
+    var i = 1; //position in the command
+
+    var numDice = "";
+    var typeDice = "";
+    var modifier = "";
+    var addSub; //true if addition, false if subtraction
+    
+
+    while (i < cmdLength && data.message.charAt(i) != 'd') {
+        numDice += data.message.charAt(i);
+        i++;
+    }
+
+    i++; //moves past 'd'
+
+    while (i < cmdLength && data.message.charAt(i) != '+' && data.message.charAt(i) != '-') {
+        typeDice += data.message.charAt(i);
+        i++;
+    }
+
+    if (data.message.charAt(i) == '+') {
+        addSub = true;
+    } else if (data.message.charAt(i) == '-') {
+        addSub = false;
+    }
+
+    while (i < cmdLength) {
+        modifier += data.message.charAt(i);
+        i++;
+    }
+
+    var numDice = parseInt(numDice);
+    var typeDice = parseInt(typeDice);
+    var modifier = parseInt(modifier);
+
+    var roll = 0;
+    for (var i = 0; i < numDice; i++) {
+        roll += Math.ceil(typeDice * Math.random());
+    }
+
+    if (typeof addSub === 'undefined') {
+        //do nothing :)
+    } else if (addSub === true) {
+        output.innerHTML += '<p><strong>DiceBot</strong>:  Rolled: ' + roll + '+' + modifier + '</p>';
+        roll += modifier;
+    } else if (addSub === false) {
+        output.innerHTML += '<p><strong>DiceBot</strong>:  Rolled: ' + roll + '' + modifier + '</p>';
+        roll += modifier;
+    }
+
+    output.innerHTML += '<p><strong>DiceBot</strong>:  Total: ' + roll + '</p>';
+}
